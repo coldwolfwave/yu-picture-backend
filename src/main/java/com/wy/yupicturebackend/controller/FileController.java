3d4bee667ec9f46a7095ae20f7776c21ebb5d63b
coldwolfwave <2064975715@qq.com>
2025-03-20 19:19:14 +0800

    增加图片上传测试接口

---
+++ src/main/java/com/wy/yupicturebackend/controller/FileController.java
@@ -0,0 +1,59 @@
+package com.wy.yupicturebackend.controller;
+
+import com.wy.yupicturebackend.annotation.AuthCheck;
+import com.wy.yupicturebackend.common.BaseResponse;
+import com.wy.yupicturebackend.common.ResultUtils;
+import com.wy.yupicturebackend.constant.UserConstant;
+import com.wy.yupicturebackend.exception.BusinessException;
+import com.wy.yupicturebackend.exception.ErrorCode;
+import com.wy.yupicturebackend.manager.CosManager;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.annotation.Resource;
+import java.io.File;
+
+
+@Slf4j
+public class FileController {
+
+    @Resource
+    private CosManager cosManager;
+
+    /**
+     * 测试文件上传
+     *
+     * @param multipartFile
+     * @return
+     */
+    @AuthCheck(mustRole = UserConstant.ADMIN_ROLE)
+    @PostMapping("/test/upload")
+    public BaseResponse<String> testUploadFile(@RequestPart("file") MultipartFile multipartFile) {
+        // 文件目录
+        String filename = multipartFile.getOriginalFilename();
+        String filepath = String.format("/test/%s", filename);
+        File file = null;
+        try {
+            // 上传文件
+            file = File.createTempFile(filepath, null);
+            multipartFile.transferTo(file);
+            cosManager.putObject(filepath, file);
+            // 返回可访问地址
+            return ResultUtils.success(filepath);
+        } catch (Exception e) {
+            log.error("file upload error, filepath = " + filepath, e);
+            throw new BusinessException(ErrorCode.SYSTEM_ERROR, "上传失败");
+        } finally {
+            if (file != null) {
+                // 删除临时文件
+                boolean delete = file.delete();
+                if (!delete) {
+                    log.error("file delete error, filepath = {}", filepath);
+                }
+            }
+        }
+    }
+
+}
